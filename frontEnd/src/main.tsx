74cdcfdb6de1862c2aa5673889d97562e113fe07
LudmiM <ludmi.m.1109@gmail.com>
2024-12-29 12:12:46 -0300

    Complete tailwind CSS configuration

--- frontEnd/src/main.tsx
+++ frontEnd/src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <h1 className='bg-red-600'>Tailwind funcionaa!</h1>
   </StrictMode>,
 )
