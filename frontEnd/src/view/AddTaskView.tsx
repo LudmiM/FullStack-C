c7979095cb3b52d37cffb0d4d8a4b6ffa7849c47
LudmiM <ludmi.m.1109@gmail.com>
2024-12-29 12:45:49 -0300

    Add required views

---
+++ frontEnd/src/view/AddTaskView.tsx
@@ -0,0 +1,7 @@
+export default function AddTaskView (){
+    return(
+        <div>
+        
+        </div>
+    )
+}
---
+++ frontEnd/src/view/DetailTaskView.tsx
@@ -0,0 +1,7 @@
+export default function EditTaskView (){
+    return(
+        <div>
+        
+        </div>
+    )
+}
